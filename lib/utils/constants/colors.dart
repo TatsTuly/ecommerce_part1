e83899c6be7dd611fe8489abe9e2410aaa1ee154
TatsTuly <tuly15-4902@diu.edu.bd>
2025-03-11 10:06:17 +0600

    Initial

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Fcolors {
+  static const primaryGreenOcean = Color(0xFF5CC936);
+  static const primaryOrange = Color(0xFFFFC120);
+  static const primaryNavyBlack = Color(0xFF0C1A30);
+  static const primarywHITE=Color(0xFFFFFFFF)
+}
